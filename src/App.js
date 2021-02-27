95e7ff890bc038c346d31439c7830046b91c7bee
khan7171 <sbabar950@gmail.com>
2021-02-27 06:37:06 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Room from './room'
 function App() {
   return (
     <div style={{width: '60%', marginLeft: "auto", marginRight: "auto"}}>
+      <h1>This was added after auto Deployment enabled</h1>
       <Room />
     </div>
   );
