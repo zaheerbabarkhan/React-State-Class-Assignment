dab692ac94106891d57e42b69d8b96a44a82d032
khan7171 <sbabar950@gmail.com>
2021-02-27 03:24:45 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Room from './room'
+function App() {
+  return (
+    <div style={{width: '60%', marginLeft: "auto", marginRight: "auto"}}>
+      <Room />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/room.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import lighton from './lighton.jpg'
+import lightoff from './lightoff.png'
+import './room.css'
+function Room() {
+
+    // const state = React.useState(100);
+    // console.log(state);
+    const [isLit, setLit] = React.useState(true);
+    // console.log(isLit);
+    return (
+    <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+        <div>
+            <img src={isLit ? `${lighton}` : `${lightoff}`} height={300} width={'100%'} />
+        </div>
+        <div className="buttons">
+            <button onClick={() => setLit(true)}>ON</button>
+            <button onClick={() => setLit(false)}>OFF</button>
+        </div>
+    </div>
+    );
+}
+
+export default Room;
